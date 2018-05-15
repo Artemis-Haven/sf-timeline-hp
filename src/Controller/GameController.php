3432575f0c8318777c494fa6d59e47f14fbe2264
Rémi Patrizio <remi.patrizio@alternative-courtage.fr>
2018-05-15 17:53:38 +0200

    Join, quit and start a new game

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Game;
+use App\Entity\Board;
+use App\Entity\Hand;
+use App\Entity\Deck;
 
 class GameController extends Controller
 {
@@ -35,7 +38,7 @@ class GameController extends Controller
 	        $em->persist($newGame);
 	        $em->flush();
 
-	        return $this->redirectToRoute('game');
+	        return $this->redirectToRoute('index');
 	    }
 
         return [
@@ -43,13 +46,77 @@ class GameController extends Controller
         	'newGameForm' => $newGameForm->createView()
         ];
     }
+    
     /**
-     * @Route("/game/{id}", name="game")
+     * @Route("/game-{id}", name="game")
 	 * @ParamConverter("game", class="App:Game")
      * @Template
      */
     public function game(Game $game)
     {
         return ['game' => $game];
+    }
+    
+    /**
+     * @Route("/start-game-{id}", name="start_game")
+	 * @ParamConverter("game", class="App:Game")
+     * @Template
+     */
+    public function start(Game $game)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	if (!$game->getStarted() && !$game->getEnded()) {
+    		$game->setStarted(true);
+    		$board = new Board();
+    		$game->setBoard($board);
+    		$em->persist($board);
+    		$deck = new Deck();
+    		$game->setDeck($deck);
+    		$em->persist($deck);
+    		foreach ($game->getMembers() as $user) {
+    			$hand = new Hand();
+    			$user->addHand($hand);
+    			$game->addHand($hand);
+    			$em->persist($hand);
+    		}
+    		$em->flush();
+    	}
+
+        return $this->redirectToRoute('game', ['id' => $game->getId()]);
+    }
+    
+    /**
+     * @Route("/join-game-{id}", name="join_game")
+	 * @ParamConverter("game", class="App:Game")
+     * @Template
+     */
+    public function join(Game $game)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	if (!$game->getStarted() && !$game->getEnded()) {
+    		$game->addMember($this->getUser());
+    		$em->flush();
+    	}
+
+        return $this->redirectToRoute('game', ['id' => $game->getId()]);
+    }
+    
+    /**
+     * @Route("/quit-game-{id}", name="quit_game")
+	 * @ParamConverter("game", class="App:Game")
+     * @Template
+     */
+    public function quit(Game $game)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	if (!$game->getStarted() && !$game->getEnded()) {
+    		$game->removeMember($this->getUser());
+    		if ($game->getMembers()->isEmpty()) {
+    			$em->remove($game);
+    		}
+    		$em->flush();
+    	}
+
+        return $this->redirectToRoute('index');
     }
 }

f9a93b39b92ba7b7eebc2aa1e8fefc0a840573a9
Rémi Patrizio <remi.patrizio@alternative-courtage.fr>
2018-05-16 12:03:49 +0200

    Added a real-time chat (through Websocket) in the game page
    Source : https://afsy.fr/avent/2017/21-symfony-et-websockets

--- assets/js/sf-websocket.js
+++ assets/js/sf-websocket.js
@@ -1,23 +1,72 @@
+/* globals wsUrl: true */
 (function () {
   'use strict';
 
   var _receiver = document.getElementById('ws-content-receiver');
   var ws = new WebSocket('ws://' + wsUrl);
+  var defaultChannel = 'general';
+  var botName = 'ChatBot';
+
+  var addMessageToChannel = function(message) {
+    _receiver.innerHTML += '<div class="message">' + message + '</div>';
+  };
+
+  var botMessageToGeneral = function (message) {
+    return addMessageToChannel(JSON.stringify({
+      action: 'message',
+      channel: defaultChannel,
+      user: botName,
+      message: message
+    }));
+  };
 
   ws.onopen = function () {
-    ws.send('Hello');
-    _receiver.innerHTML = 'Connected !';
+    ws.send(JSON.stringify({
+      action: 'subscribe',
+      channel: defaultChannel,
+      user: userName
+    }));
   };
 
   ws.onmessage = function (event) {
-    _receiver.innerHTML = event.data;
+    addMessageToChannel(event.data);
   };
 
   ws.onclose = function () {
-    _receiver.innerHTML = 'Connection closed';
+    botMessageToGeneral('Connection closed');
   };
 
   ws.onerror = function () {
-    _receiver.innerHTML = 'An error occured!';
+    botMessageToGeneral('An error occured!');
   };
-})();
+
+
+
+  var _textInput = document.getElementById('ws-content-to-send');
+  var _textSender = document.getElementById('ws-send-content');
+  var enterKeyCode = 13;
+
+  var sendTextInputContent = function () {
+    // Get text input content
+    var content = _textInput.value;
+
+    // Send it to WS
+    ws.send(JSON.stringify({
+      action: 'message',
+      user: userName,
+      message: content,
+      channel: 'general'
+    }));
+
+    // Reset input
+    _textInput.value = '';
+  };
+
+  _textSender.onclick = sendTextInputContent;
+  _textInput.onkeyup = function(e) {
+    // Check for Enter key
+    if (e.keyCode === enterKeyCode) {
+      sendTextInputContent();
+    }
+  };
+}) ();
--- src/Server/Chat.php
+++ src/Server/Chat.php
@@ -8,22 +8,37 @@ use Ratchet\MessageComponentInterface;
 class Chat implements MessageComponentInterface
 {
     private $clients;
+    private $users = [];
+    private $botName = 'ChatBot';
+    private $defaultChannel = 'general';
 
     public function __construct()
     {
         $this->clients = new \SplObjectStorage();
+        echo "Listening...\n";
     }
 
     public function onOpen(ConnectionInterface $conn)
     {
         $this->clients->attach($conn);
-        $conn->send(sprintf('New connection: Hello #%d', $conn->resourceId));
+        $conn->send(json_encode([
+            'action'  => 'message',
+            'channel' => $this->defaultChannel,
+            'user'    => $this->botName,
+            'message' => sprintf('Connection established. Welcome #%d!', $conn->resourceId),
+        ]));
+        $this->users[$conn->resourceId] = [
+            'connection' => $conn,
+            'user' => '',
+            'channels' => []
+        ];
     }
 
     public function onClose(ConnectionInterface $closedConnection)
     {
         $this->clients->detach($closedConnection);
         echo sprintf('Connection #%d has disconnected\n', $closedConnection->resourceId);
+        unset($this->users[$closedConnection->resourceId]);
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e)
@@ -32,20 +47,75 @@ class Chat implements MessageComponentInterface
         $conn->close();
     }
 
-    public function onMessage(ConnectionInterface $from, $message)
+    public function onMessage(ConnectionInterface $conn, $message)
     {
-        $totalClients = count($this->clients) - 1;
-        echo vsprintf(
-            'Connection #%1$d sending message "%2$s" to %3$d other connection%4$s'."\n", [
-            $from->resourceId,
-            $message,
-            $totalClients,
-            $totalClients === 1 ? '' : 's'
-        ]);
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                $client->send($message);
+        $messageData = json_decode($message);
+        if ($messageData === null) {
+            return false;
+        }
+
+        echo $message.'\n';
+        $action = $messageData->action ?? 'unknown';
+        $channel = $messageData->channel ?? $this->defaultChannel;
+        $user = $messageData->user ?? $this->botName;
+        $message = $messageData->message ?? '';
+
+        switch ($action) {
+            case 'subscribe':
+                $this->subscribeToChannel($conn, $channel, $user);
+                return true;
+            case 'unsubscribe':
+                $this->unsubscribeFromChannel($conn, $channel, $user);
+                return true;
+            case 'message':
+                return $this->sendMessageToChannel($conn, $channel, $user, $message);
+            default:
+                echo sprintf('Action "%s" is not supported yet!', $action);
+                break;
+        }
+        return false;
+    }
+
+    private function subscribeToChannel(ConnectionInterface $conn, $channel, $user)
+    {
+        $this->users[$conn->resourceId]['channels'][$channel] = $channel;
+        $this->sendMessageToChannel(
+            $conn,
+            $channel,
+            $this->botName,
+            $user.' joined #'.$channel
+        );
+    }
+
+    private function unsubscribeFromChannel(ConnectionInterface $conn, $channel, $user)
+    {
+        if (array_key_exists($channel, $this->users[$conn->resourceId]['channels'])) {
+            unset($this->users[$conn->resourceId]['channels']);
+        }
+        $this->sendMessageToChannel(
+            $conn,
+            $channel,
+            $this->botName,
+            $user.' left #'.$channel
+        );
+    }
+
+    private function sendMessageToChannel(ConnectionInterface $conn, $channel, $user, $message)
+    {
+        if (!isset($this->users[$conn->resourceId]['channels'][$channel])) {
+            return false;
+        }
+       foreach ($this->users as $connectionId => $userConnection) {
+            if (array_key_exists($channel, $userConnection['channels'])) {
+                $userConnection['connection']->send(json_encode([
+                    'action' => 'message',
+                    'channel' => $channel,
+                    'user' => $user,
+                    'message' => $message
+                ]));
             }
         }
+        return true;
     }
+
 }
